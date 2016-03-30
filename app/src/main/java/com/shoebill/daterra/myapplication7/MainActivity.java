984d3e6dc982b5de973acae07f4f10131b65aeae
yukichikun <daterra.shoebill.pallas@gmail.com>
2016-03-30 19:06:27 +0900

    first commit

---
+++ app/src/main/java/com/shoebill/daterra/myapplication7/MainActivity.java
@@ -0,0 +1,20 @@
+package com.shoebill.daterra.myapplication7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ボタンを設定
+
+        Button button =  (Button) findViewById(R.id.buttonA);
+    }
+}
